46b607e8fa3f9420c329a183ecad67bb9d8ce1bc
ravikajan <61968423+ravikajan@users.noreply.github.com>
2022-09-25 10:18:52 +0100

    image fixed

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
+    domains: ["s3.eu-west-2.amazonaws.com"],
   },
 })
 
